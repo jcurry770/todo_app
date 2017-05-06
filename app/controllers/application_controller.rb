0f82604be2e43d3c2a7372167da760bc49036c6b
James Curry <jcurryiv2020@erhsnyc.net>
2017-05-06 16:07:43 +0000

    Fixed root page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-end
-def hello
-  render html: “Whatever message you want!”
+  def hello
+    render html: 'Whatever message you want!'
+  end
 end
